cc6a4b058a2f697f12658edf5f85a1005febc2ad
Sam H <samuelhussain@live.com>
2023-02-04 16:25:30 -0500

    Co-authored-by: zeyujiang8800 <zeyujiang8800@users.noreply.github.com> Co-authored-by: Jack <GeaugaJack@users.noreply.github.com> Co-authored-by: Seoyoung Kong <promise310@users.noreply.github.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
-  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+      .then((_) {
+    runApp(const MyApp());
+  });
+}
+
+class FoodItem {
+  String name;
+  int quantity;
+  FoodItem(this.name, this.quantity);
+
+  String toString() {
+    return 'FoodItem{name: $name, quantity: $quantity}';
+  }
 }
 
 class MyApp extends StatelessWidget {
@@ -12,7 +27,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Knightro Rice',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.lightGreen,
       ),
       home: const MyHomePage(title: 'Knightro Rice'),
     );
@@ -28,46 +43,118 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int currentIndex = 0;
+  var quantityTotal = 0;
+  var kcTotal = 0;
+  List<FoodItem> donationList = [];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: NavigationBar(
-        onDestinationSelected: (int index) {
-          setState(() {
-            currentIndex = index;
-          });
-        },
-        selectedIndex: currentIndex,
-        destinations: const <Widget>[
-          NavigationDestination(icon: Icon(Icons.list), label: 'Donations'),
-          NavigationDestination(icon: Icon(Icons.star), label: 'Rewards'),
+      appBar: (AppBar(title: const Text('Knightro Rice'))),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          Expanded(
+            child: ListView.builder(
+              padding: const EdgeInsets.all(8),
+              itemCount: donationList.length,
+              itemBuilder: (BuildContext context, int i) {
+                return Card(
+                  margin: const EdgeInsets.all(10),
+                  child: ListTile(
+                    title: Text(
+                      'Item: ${donationList[i].name}',
+                      style: const TextStyle(
+                          //style change
+                          fontSize: 26),
+                    ),
+                    subtitle: Text(
+                      'Quantity: ${donationList[i].quantity.toString()}',
+                      style: const TextStyle(
+                          //style change
+                          fontSize: 20),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.all(20),
+            child: SizedBox(
+              width: MediaQuery.of(context).size.width * 0.8,
+              height: 75,
+              child: ElevatedButton(
+                onPressed: () => addDonationDialog(context),
+                style: ElevatedButton.styleFrom(
+                  textStyle: const TextStyle(fontSize: 42),
+                ),
+                child: const Text("Add Donation"),
+              ),
+            ),
+          ),
         ],
       ),
-      appBar: (AppBar(title: const Text('Donations'))),
-      body: <Widget>[
-        Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    );
+  }
+
+  void addDonationDialog(BuildContext context) {
+    var quantityController = TextEditingController();
+    var itemController = TextEditingController();
+    const errorBar = SnackBar(content: Text("Invalid Input"));
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text("Add Donation Item"),
+          content: Column(
             children: <Widget>[
-              const Text('Donations'),
-              SizedBox(
-                width: MediaQuery.of(context).size.width * 0.8,
-                height: 75,
-                child: ElevatedButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                    textStyle: const TextStyle(fontSize: 42),
-                  ),
-                  child: const Text("Add Donation"),
+              TextField(
+                decoration: const InputDecoration(
+                  labelText: "Item Name",
                 ),
+                controller: itemController,
               ),
+              TextField(
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(
+                    labelText: "Quantity",
+                  ),
+                  controller: quantityController),
             ],
           ),
-        ),
-        const Text('Rewards'),
-      ][currentIndex],
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: const Text("Cancel"),
+            ),
+            TextButton(
+              onPressed: () => {
+                if (itemController.text.isNotEmpty &&
+                    quantityController.text.isNotEmpty &&
+                    RegExp(r'^-?[0-9]+$').hasMatch(quantityController.text))
+                  {
+                    setState(() {
+                      quantityTotal += int.parse(quantityController.text);
+                      if (quantityTotal % 5 == 0) {
+                        kcTotal++;
+                      }
+                      print('Quantity: $quantityTotal');
+                      print('KC: $kcTotal');
+                    }),
+                    donationList.add(FoodItem(itemController.text,
+                        int.parse(quantityController.text))),
+                    for (var item in donationList) {print(item)},
+                    Navigator.pop(context)
+                  }
+                else
+                  {ScaffoldMessenger.of(context).showSnackBar(errorBar)}
+              },
+              child: const Text("Submit"),
+            ),
+          ],
+        );
+      },
     );
   }
 }
