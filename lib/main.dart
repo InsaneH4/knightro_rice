034305e67c5fbf6c2c954c503b15d57a166de2df
Sam H <samuelhussain@live.com>
2023-02-04 12:50:47 -0500

    Co-authored-by: Jack <GeaugaJack@users.noreply.github.com> Co-authored-by: zeyujiang8800 <zeyujiang8800@users.noreply.github.com> Co-authored-by: Seoyoung Kong <promise310@users.noreply.github.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'donations.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +14,53 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const Donations(),
+      home: const MyHomePage(title: 'Knightro Rice'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int currentIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: NavigationBar(
+        onDestinationSelected: (int index) {
+          setState(() {
+            currentIndex = index;
+          });
+        },
+        selectedIndex: currentIndex,
+        destinations: const <Widget>[
+          NavigationDestination(icon: Icon(Icons.list), label: 'Donations'),
+          NavigationDestination(icon: Icon(Icons.star), label: 'Rewards'),
+        ],
+      ),
+      appBar: (AppBar(title: const Text('Donations'))),
+      body: <Widget>[
+        Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              const Text('Donations'),
+              ElevatedButton(
+                onPressed: () {},
+                child: const Text("Add Donation"),
+              ),
+            ],
+          ),
+        ),
+        const Text('Rewards'),
+      ][currentIndex],
     );
   }
 }
